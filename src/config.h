3de1004d0214f75bf42bfa2907f382e02192b667
bob128730 <76489670+bob128730@users.noreply.github.com>
2026-06-04 18:23:31 +0100

    Auto create config if not present

--- src/config.h
+++ src/config.h
@@ -1,17 +1,47 @@
 #include "INIReader.h"
 
+const char* defaultConfigFile =
+"[Config]\n\
+DisableJumpCam = true\n\n\
+[Compatibility]\n\
+GravLanesSupport = false\n\0";
+
 struct config 
 {
 	void load() 
 	{
-		INIReader reader("Data\\SFSE\\plugins\\SGJ_Config.ini");
-		if (reader.ParseError() != 0) 
+		FILE* file;
+		const char* path = "Data\\SFSE\\plugins\\SGJ_Config.ini";
+
+   		file = fopen(path, "r");
+
+		if (!file)
 		{
-			REX::WARN("Failed to read config file, using default");
-			return;
+			REX::INFO("No config file, creating with defaults");
+			file = fopen(path, "w+");
+			fprintf(file, "%s", defaultConfigFile);
+			fclose(file);
+
+			file = fopen(path, "r");
 		}
-		this->DisableJumpCam = reader.GetBoolean("Config", "DisableJumpCam", true);
-		this->GravLanesSupport = reader.GetBoolean("Compatibility", "GravLanesSupport", false);
+		if (file)
+		{
+			INIReader reader(file);
+			if (reader.ParseError() != 0)
+			{
+				REX::WARN("Failed to read config file, using default");
+				return;
+			}
+			this->DisableJumpCam = reader.GetBoolean("Config", "DisableJumpCam", true);
+			this->GravLanesSupport = reader.GetBoolean("Compatibility", "GravLanesSupport", false);
+
+			fclose(file);
+		}
+
+		REX::INFO("Config:");
+		REX::INFO("DisableJumpCam: {}", this->DisableJumpCam);
+		REX::INFO("GravLanesSupport: {}", this->GravLanesSupport);
+		REX::INFO("-----------------------------------------------------------------");
 	}
 
 	bool DisableJumpCam = 1;
